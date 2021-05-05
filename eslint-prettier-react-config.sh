f2c4b25cdfe4a7118b2397ba2bdb133a261bfce7
Bern <bernnystrom@gmail.com>
2021-05-06 03:28:05 +0800

    Initial commit with color variables set for cmd prompt messages

---
+++ eslint-prettier-react-config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# ----------------------
+# Color Variables
+# ----------------------
+RED="\033[0;31m"
+YELLOW='\033[1;33m'
+GREEN='\033[1;32m'
+LCYAN='\033[1;36m'
+NC='\033[0m' # No Color
+
+# --------------------------------------
+# Prompts for configuration preferences
+# --------------------------------------
+
+# Package Manager Prompt
